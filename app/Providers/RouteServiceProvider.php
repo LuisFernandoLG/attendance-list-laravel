eb3a74335de3026bd9e54b3aa5e15553d2c64d90
LuisFernandoLG <luislopez1099lg@gmail.com>
2024-03-27 17:06:08 -0700

    feat: V1 added because of vercel issues with the path API

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -30,7 +30,7 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->routes(function () {
             Route::middleware('api')
-                ->prefix('api')
+                ->prefix('apiV1')
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
