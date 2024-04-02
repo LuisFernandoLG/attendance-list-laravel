f1092b861d2d85326085fb4c4eb79332f86062dd
LuisFernandoLG <luislopez1099lg@gmail.com>
2024-04-02 11:50:50 -0700

    feat: get attendance list with params and filters

--- app/Http/Controllers/EventAttendanceController.php
+++ app/Http/Controllers/EventAttendanceController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\ControlledListRecord;
 use App\Models\Event;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class EventAttendanceController extends Controller
 {
@@ -14,6 +16,7 @@ class EventAttendanceController extends Controller
     public function show(Request $request, $id){
 
         $event = Event::where('id', $id)->where('user_id', request()->user()->id)->first();
+        $userTimezone = $request->user()->timezone;
 
         if(!$event){
             return response()->json([
@@ -21,12 +24,44 @@ class EventAttendanceController extends Controller
             ], 404);
         }
 
-        $query = ControlledListRecord::query();
-        $pagination = $query->where('event_id', $id)->with("member")->orderBy('created_at', 'desc')->paginate($this->membersPerPage);
+        $now = Carbon::now()->timezone($userTimezone)->format('Y-m-d');
+        $date = $request->has('date') ? Carbon::createFromDate($request->date)->format('Y-m-d') : $now;
+
+        config()->set('database.connections.mysql.strict', false);
+        DB::reconnect();
+        
+        $query = ControlledListRecord::query()->with('member')->where('event_id', $id)->whereDate('created_at', $date);
+
+
+        if($request->has('search')){
+            $query->whereHas('member', function($q) use ($request){
+                $q->where('name', 'like', '%'.$request->search.'%');
+            });
+        }
+        
+        if($request->has('order')){
+            if($request->order == 'asc'){
+                $query->orderBy('created_at', 'asc');
+            }
+        }else{
+            $query->orderBy('created_at', 'desc');
+        }
+        
+        $perPage = $request->has('perPage') ? $request->perPage : $this->membersPerPage;
+        
+        $pagination = $query->
+                            select('event_id', 'member_id', DB::raw('MAX(created_at) as last_attendance, COUNT(*) as attendances'))
+                                        ->groupBy('member_id')
+                                        ->paginate($perPage);
+
+        config()->set('database.connections.mysql.strict', true);
+        DB::reconnect();
 
         return response()->json([
+            'date' => $date,
+            '$userTimezone' => $userTimezone,
             'message' => 'item retrieved successfully',
-            'pagination' => $pagination
+            'pagination' => $pagination,
         ]);
     }
 }
